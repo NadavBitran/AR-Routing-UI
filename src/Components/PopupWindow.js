afbed1b0a432797b7143bfd60ea5832cd2765f65
Nadav <nadavbitran@gmail.com>
2023-08-28 12:30:33 +0300

    Added Interactive Tutorial Component

--- src/Components/PopupWindow.js
+++ src/Components/PopupWindow.js
@@ -5,6 +5,7 @@ import questionSVG from "../Assets/images/question-circle.svg";
 import infoSVG from "../Assets/images/info-circle.svg";
 import warningSVG from "../Assets/images/warning-circle.svg";
 
+
 // ---- PROPS ----
 // type: string that contains the type of the popup window
 // title: string that contains the title of the popup window
@@ -12,7 +13,8 @@ import warningSVG from "../Assets/images/warning-circle.svg";
 // buttonsKey: array of strings that contains the keys of the buttons
 // buttonsContent: array of strings that contains the content of the buttons
 // setUserDecision: function that changes the value of the userDecision state in the parent component
-export default function PopupWindow({type, title, mainContent, buttonsKey, buttonsContent, setUserDecision }) {
+// imageDetails: a object that contains ref to image and alt text
+export default function PopupWindow({type, title, mainContent, buttonsKey, buttonsContent, setUserDecision , imageDetails = null }) {
     // ---- STATE HOOKS ----
     // isOpen: boolean that indicates if the popup window is open or not
     const [isOpen, setIsOpen] = useState(true);
@@ -28,6 +30,7 @@ export default function PopupWindow({type, title, mainContent, buttonsKey, butto
         "info": "Information mark",
         "warning": "Warning mark"
     }
+
     const popupWindowType = Object.keys(popupWindowIcons).includes(type) ? type : "question";
     const popupWindowIcon = Object.keys(popupWindowIcons).includes(type) ? popupWindowIcons[type] : popupWindowIcons["question"];
     const popupWindowIconAlt = Object.keys(popupWindowIconAlts).includes(type) ? popupWindowIconAlts[type] : popupWindowIconAlts["question"];
@@ -51,6 +54,7 @@ export default function PopupWindow({type, title, mainContent, buttonsKey, butto
                             <h2 className="popup-window__title">{title}</h2> {/* The title of the popup window */}
                         </header>
                         <p className="popup-window__main-content">{mainContent}</p> {/* The main content of the popup window */}
+                        {imageDetails!==null && <img src={imageDetails.image} alt={imageDetails.alt}/>}
                         <div className={"popup-window__buttons"}> {/* The buttons of the popup window */}
                             {buttonsContent.map((content, index) => ( // For each button, a button element is rendered 
                                 <button className={"popup-window__button--" + type} 
