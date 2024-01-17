ea463a14ca28124b70cb92c2d8d7c8a1fd1b9cad
Maor <maorbz858@gmail.com>
2024-01-17 20:53:47 +0200

    Add new type definitions & move existing types into ts files

---
+++ src/common/types/data.types.ts
@@ -0,0 +1,120 @@
+/**
+ * @module data.types.ts
+ * @description contains all the types used to represent the different data used throughout the application.
+ *
+ * @author Maor Bezalel
+ */
+
+/**
+ * @description represents a latitude and longitude coordinate pair.
+ * @remarks accepts either an object with `lat` and `lng` properties or an array with the latitude and longitude values in that order.
+ * @example
+ * ```ts
+ * const a: LatLngExpression = { lat: 0, lng: 0};
+ * const b: LatLngExpression = [0, 0]; // b[0] = lat, b[1] = lng
+ * ```
+ */
+export type LatLngExpression = import('leaflet').LatLngExpression;
+
+/**
+ * @description represents a pixel value in a string format with a `px` suffix.
+ * @example
+ * ```ts
+ * const a: Pixels = '10px'; // correct!
+ * const b: Pixels = 10; // incorrect! isn't a string with a `px` suffix
+ * const c: Pixels = '10'; // incorrect! doesn't have a `px` suffix
+ * const d = 'somethingpx'; // incorrect! doesn't have a number at the beginning
+ * ```
+ */
+export type Pixels = `${number}px`;
+
+/** @description an object representing a route which is made up of steps with direction and length. */
+export type Route = {
+    /** @description the unique identifier of the route which helps React to distinguish between different rendered steps. */
+    id: string;
+
+    /** @description the name of the route. given by the user. */
+    name: string;
+
+    /** @description whether the route is currently selected (via a checkbox). */
+    isChecked: boolean;
+
+    /** @description whether the route is currently expanded to show its steps. */
+    isExpanded: boolean;
+
+    /** @description the steps that make up the route. */
+    steps: Step[];
+};
+
+/** @description an object representing a step which belong to a specific route.*/
+export type Step = {
+    /** @description the unique identifier of the step which helps React to distinguish between different rendered steps. */
+    id: string;
+
+    /** @description the length (in meters) of the step. */
+    length: number;
+
+    /** @description the direction of the step. */
+    direction: Direction;
+
+    /** @description whether the step is currently selected (via a checkbox). */
+    isChecked: boolean;
+};
+
+/** @description the different directions a step can have. */
+export type Direction =
+    | 'Foward'
+    | 'Forward-Left'
+    | 'Forward-Right'
+    | 'Left'
+    | 'Right'
+    | 'Backward'
+    | 'Backward-Left'
+    | 'Backward-Right';
+
+/** @description an object representing the essential data for rendering an image. */
+export type Image = {
+    /** @description the source of the image. */
+    src: string;
+
+    /** @description the alternative text of the image. */
+    alt: string;
+};
+
+/** @description an object representing the essential data for rendering a button. */
+export type Button = {
+    /** @description the text that will appear on the button. */
+    text: string;
+
+    /** @description the function that will be called when the button is clicked. */
+    action: () => void;
+};
+
+/**
+ * @description represents where a tooltip should be positioned relative to its target.
+ * @remarks used to render a specific tooltip css class.
+ */
+export type TooltipPositionRelativeToTarget =
+    | 'top'
+    | 'topRight'
+    | 'topLeft'
+    | 'right'
+    | 'buttomRight'
+    | 'buttom'
+    | 'buttomLeft'
+    | 'left';
+
+/** @description an object representing the coordinates of a tooltip in pixels. */
+export type TooltipCoordinates = {
+    /** @description the height of the tooltip in pixels. */
+    height: Pixels;
+
+    /** @description the width of the tooltip in pixels. */
+    width: Pixels;
+
+    /** @description the middle x coordinate of the tooltip in pixels. */
+    middleX: Pixels;
+
+    /** @description the middle y coordinate of the tooltip in pixels. */
+    middleY: Pixels;
+};
---
+++ src/common/types/hooks-related.types.ts
@@ -0,0 +1,212 @@
+/**
+ * @module types/hooks-related
+ * @description contains all the types used to represent hooks-related data and functions.
+ *
+ * @author Maor Bezalel
+ */
+
+import { LatLngExpression, Direction, Route } from './data.types';
+
+/**
+ * @description the type of the function that sets state returned by React's `useState` hook.
+ * @template TState - the type of the state
+ */
+export type SetState<TState> = import('react').Dispatch<
+    import('react').SetStateAction<TState>
+>;
+
+/**
+ * @description the return type of the `useMarkerLocation` hook.
+ */
+export type UseMarkerLocationResults = {
+    /** @description the current location of the marker. */
+    markerLocation: LatLngExpression;
+
+    /** @description a function that updates the location of the marker. */
+    updateMarkerLocation: (
+        latlng: LatLngExpression,
+        updatedFrom: string
+    ) => void;
+
+    /** @description a function that resets the location of the marker to its default value. */
+    latestMarkerUpdateOperation: string;
+};
+
+/**
+ * @description Represents a set of actions that can be performed on a list of items.
+ * @template TItem - The type of items in the list.
+ * @remarks This type is used to represent one of the return values of the `useList` hook.
+ */
+export type ListActions<TItem> = {
+    /**
+     * @description Sets a new list of items in place of the old one.
+     * @param items - The new list of items.
+     */
+    set: (items: TItem[]) => void;
+
+    /**
+     * @description Adds a single or multiple items to the end of the list.
+     * @param item - The item(s) to be added.
+     */
+    add: (...item: TItem[]) => void;
+
+    /**
+     * @description Adds a single or multiple items at the specified index.
+     * @param index - The index at which the items should be added.
+     * @param item - The item(s) to be added.
+     */
+    addAt: (index: number, ...item: TItem[]) => void;
+
+    /**
+     * @description Removes a single or multiple items from the list at the specified index(es).
+     * @param indices - The indices of the items to be removed.
+     */
+    removeAt: (...indices: number[]) => void;
+
+    /**
+     * @description Updates a single item in the list at the specified index.
+     * @param item - The new value of the item.
+     * @param indices - the index of one or more items to be updated.
+     */
+    updateAt: (item: TItem, ...indices: number[]) => void;
+
+    /**
+     * @description Updates a single property of an item in the list at the specified index.
+     * @template TProp - The type of the property to update.
+     * @param prop - The property to update.
+     * @param newValue - The new value of the property.
+     * @param indices - The indices of the items to be removed.
+     * @remarks This is relevant only for items in the list that are objects.
+     */
+    updatePropAt: <TProp extends keyof TItem>(
+        prop: TProp,
+        newValue: TItem[TProp],
+        ...indices: number[]
+    ) => void;
+
+    /** @description Removes all items from the list (equivalent to setting an empty list). */
+    clear: () => void;
+};
+
+/**
+ * @description Represents a set of actions that can be performed on a list of routes.
+ * @remarks This type is used to represent one of the return values of the `useRouteList` hook.
+ */
+export type RouteListActions = {
+    /**
+     * @description sets a new list of routes in place of the old one.
+     * @param routes - The new list of routes.
+     */
+    set: (routes: Route[]) => void;
+
+    /**
+     * @description adds a single or multiple routes to the end of the list.
+     * @param amount - The number of routes to be added.
+     */
+    addRoutes: (amount: number) => void;
+
+    /**
+     * @description adds a single or multiple routes at the specified index.
+     * @param index - The index at which the routes should be added.
+     * @param amount - The number of routes to be added.
+     */
+    addRoutesAt: (index: number, amount: number) => void;
+
+    /**
+     * @description adds steps to a route at the specified index.
+     * @param routeIndex - The index of the route.
+     * @param amount - The number of steps to be added.
+     */
+    addStepsToRouteAt: (routeIndex: number, amount: number) => void;
+
+    /**
+     * @description removes a single or multiple routes from the list at the specified index(es).
+     * @param indices - The indices of the routes to be removed.
+     */
+    removeRoutesAt: (...indices: number[]) => void;
+
+    /**
+     * @description removes steps from a route at the specified index.
+     * @param routeIndex - The index of the route.
+     * @param stepIndices - The indices of the steps to be removed.
+     */
+    removeStepsFromRouteAt: (
+        routeIndex: number,
+        ...stepIndices: number[]
+    ) => void;
+
+    /** @description removes all checked routes and steps from the list. */
+    removeAllCheckedRoutesAndSteps: () => void;
+
+    /**
+     * @description updates the name of a route at the specified index.
+     * @param index - The index of the route.
+     * @param name - The new name of the route.
+     */
+    updateRouteNameAt: (index: number, name: string) => void;
+
+    /**
+     * @description updates the check status of routes at the specified indices.
+     * @param checkStatus - The new check status.
+     * @param routeIndices - The indices of the routes to be updated.
+     */
+    updateRoutesCheckStatusAt: (
+        checkStatus: boolean,
+        ...routeIndices: number[]
+    ) => void;
+
+    /**
+     * @description updates the expansion status of a route at the specified index.
+     * @param index - The index of the route.
+     * @param expansionStatus - The new expansion status.
+     */
+    updateRouteExpansionStatusAt: (
+        index: number,
+        expansionStatus: boolean
+    ) => void;
+
+    /**
+     * @description updates the direction of a step in a route.
+     * @param routeIndex - The index of the route.
+     * @param stepIndex - The index of the step.
+     * @param direction - The new direction of the step.
+     */
+    updateStepDirectionAt: (
+        routeIndex: number,
+        stepIndex: number,
+        direction: Direction
+    ) => void;
+
+    /**
+     * @description updates the length of a step in a route.
+     * @param routeIndex - The index of the route.
+     * @param stepIndex - The index of the step.
+     * @param length - The new length of the step.
+     */
+    updateStepLengthAt: (
+        routeIndex: number,
+        stepIndex: number,
+        length: number
+    ) => void;
+
+    /**
+     * @description updates the check status of steps in a route.
+     * @param checkStatus - The new check status.
+     * @param routeIndex - The index of the route.
+     * @param stepIndices - The indices of the steps to be updated.
+     */
+    updateStepsCheckStatusAt: (
+        checkStatus: boolean,
+        routeIndex: number,
+        ...stepIndices: number[]
+    ) => void;
+
+    /** @description checks all routes. */
+    checkAllRoutes: () => void;
+
+    /** @description unchecks all routes. */
+    uncheckAllRoutes: () => void;
+
+    /** @description Removes all routes and steps from the list. */
+    clear: () => void;
+};
